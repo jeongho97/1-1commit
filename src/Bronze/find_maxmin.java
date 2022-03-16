2db4c72fdf6271c7e5ab8c1d201b4bce2e992216
jeongho97 <skjongho@naver.com>
2022-03-16 14:18:35 +0900

    2022-03-16

---
+++ src/Bronze/find_maxmin.java
@@ -0,0 +1,43 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class find_maxmin {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int num=Integer.parseInt(reader.readLine());
+		StringTokenizer st=new StringTokenizer(reader.readLine());
+		
+		String str = reader.readLine();
+		int[] arr2 = new int[num];
+		arr2=Stream.of(str.split(" ")).mapToInt(Integer::parseInt).toArray();
+	
+		
+		String str2=reader.readLine();
+		int[] arr3 = new int[str2.length()];
+		for(int i=0;i<str2.length();i++)
+		{
+			arr3[i]=str2.charAt(i)-'0';
+		}
+		
+		//System.out.println(Arrays.toString(arr2));
+		System.out.println(Arrays.toString(arr3));
+		
+		int index=0;
+		int[] arr = new int[num];
+		while(st.hasMoreTokens())
+		{
+			arr[index]=Integer.parseInt(st.nextToken());
+			index++;
+
+		}
+		Arrays.sort(arr2);
+		System.out.print(arr2[0]+" "+arr2[num-1]);
+
+	}
+}
---
+++ src/Bronze/while_1.java
@@ -0,0 +1,37 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class while_1 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+		int num=Integer.parseInt(str);
+		if(num<0||num>99)
+		{
+			System.out.println("0보다 크거나 같고 99보다 작거나 같은 정수를 입력하세요");
+		}
+		int count=0;
+		int pre_num=num;
+		while(true)
+		{
+			int ten=num/10;
+			int one=num%10;
+			int new_one=(ten+one)%10;
+			num=one*10+new_one;
+			count++;
+			if(pre_num==num)
+			{
+				break;
+			}
+		}
+		System.out.println(count);
+		
+
+	}
+
+}
