15b73556b9eb00004ffec10f59d736b96e54b8f1
jeongho97 <skjongho@naver.com>
2022-03-17 12:10:42 +0900

    2022-03-17 1차원배열

---
+++ src/Bronze/average.java
@@ -0,0 +1,50 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class average {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+		
+		int case_num = Integer.parseInt(reader.readLine());
+		int[] arr;
+		double[] arr2=new double[case_num];
+		
+		for(int i=0; i<case_num;i++)
+		{
+			int sum=0;
+			double avg=0.0;
+			StringTokenizer st = new StringTokenizer(reader.readLine());
+			int student_num=Integer.parseInt(st.nextToken());
+			arr=new int[student_num];
+			for(int j=0;j<student_num;j++)
+			{
+				arr[j]=Integer.parseInt(st.nextToken());
+				sum+=arr[j];
+			}
+			avg=(double)(sum/student_num);
+			int count=0;
+			for(int k=0;k<student_num;k++)
+			{
+				if(arr[k]>avg)
+				{
+					count++;
+				}
+			}
+			arr2[i]=(double)count/student_num*100;
+		}
+		for(int i=0;i<case_num;i++)
+		{
+			System.out.printf("%.3f%%\n",arr2[i]);
+		}
+	}
+
+}
---
+++ src/Bronze/find_max_index.java
@@ -0,0 +1,29 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class find_max_index {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int [] arr=new int[9];
+		for(int i=0;i<9;i++)
+		{
+			arr[i] =Integer.parseInt(reader.readLine()) ;
+		}
+		int max=arr[0];
+		int index=0;
+		for(int i=1;i<arr.length;i++)
+		{
+			if(max<arr[i])
+			{
+				max=arr[i];
+				index=i;
+			}
+		}
+		System.out.println(max);
+		System.out.println(index+1);
+	}
+
+}
