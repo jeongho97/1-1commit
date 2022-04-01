3be4887a41e0a0403a6d79d539c530abba65742f
jeongho97 <skjongho@naver.com>
2022-04-01 22:10:24 +0900

    통계학(정렬)_미완성

---
+++ src/Baekjoon/통계학_정렬.java
@@ -0,0 +1,68 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class 통계학_정렬 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(reader.readLine());
+		int[] arr=new int[8001];
+		int[] arrsort=new int[N];
+		
+		double sum=0;
+		for(int i=0; i<N;i++)
+		{
+			int num=Integer.parseInt(reader.readLine());
+			arr[num+4000]++;
+			sum+=num;
+			
+		}
+		System.out.printf("%.0f\n", sum/N); //산술평균
+		int i=0;
+		int max=0;
+		int mode=0;
+		for(int j=0; j<8001; j++)
+		{
+			if(arr[j]>0)
+			{
+				if(max<arr[j])
+				{
+					max=arr[j];
+				}
+				else if(max==arr[j])
+				{
+					if(max<arr[j])
+					{
+						mode=max;
+					}
+					else
+					{
+						mode=arr[j];
+					}
+					//최빈값이 여러개 있을때 최빈값 중 두 번째로 작은 값을 출력
+				}
+			}
+			while(arr[j]>0)
+			{
+				int num_result=j-4000;
+				arrsort[i]=num_result;
+				i++;
+				arr[j]--;
+			}
+		}
+
+		double median=(N/2)*1/1.0;
+		System.out.println(arrsort[(int)median]); //중앙값
+		System.out.println(mode);
+		System.out.println(arrsort[N-1]-arrsort[0]); //범위
+	}
+
+}
